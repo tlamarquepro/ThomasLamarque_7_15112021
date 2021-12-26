945b717f407dad59bf777ba9626bab034b2fa19b
Thomas Lamarque <t.lamarque.pro@outlook.fr>
2021-12-26 23:31:51 +0100

    creation de la carte de profil

--- frontend/src/components/Carte.js
+++ frontend/src/components/Carte.js
@@ -8,12 +8,16 @@ const Carte = () => {
   return (
     <div className="card">
       <div className="profile-picture">
-        <img src={`../assets/photo/${userData.picture}`} alt="" />
+        <img
+          src={`../assets/photo/${userData.picture}`}
+          alt="profil"
+          className="picture"
+        />
       </div>
-      <div className="profile-name"></div>
-      <div className="profile-firstname"></div>
-      <div className="profile-job"></div>
-      <div className="profile-bio"></div>
+      <div className="profile-name">{userData.lastname}</div>
+      <div className="profile-firstname">{userData.firstname}</div>
+      <div className="profile-job">{userData.job}</div>
+      <div className="profile-bio">{userData.bio}</div>
     </div>
   );
 };
